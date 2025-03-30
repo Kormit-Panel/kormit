7c36bc5e4924c2c7e1500beae94db5cc85039f24
hennycraft <122104659+hennycraft52@users.noreply.github.com>
2025-03-30 15:50:39 +0200

    Update

--- deploy/scripts/install.ps1
+++
@@ -1,4 +0,0 @@
-# Kormit-Installationsskript für Windows
-#
-# Version 1.1.5 - Vollständige Korrektur des Start-Skript-Problems
-# (c) 2023-2024 Kormit 
--- deploy/scripts/install_manual.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Kormit manuelle Installation - Einfacher Installer
-# Dieses Skript lädt das manuelle Installationsskript herunter und führt es aus
-
-curl -sSL https://raw.githubusercontent.com/kormit-panel/kormit/main/deploy/scripts/manual_install.sh | sudo bash 
